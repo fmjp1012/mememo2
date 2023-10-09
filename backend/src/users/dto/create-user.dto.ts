3939e517df405fc1e8c258274a3ee13fc5286ce8
fmjp1012 <fmjp1012@gmail.com>
2023-10-10 06:47:28 +0900

    Add model

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -2,8 +2,6 @@ import { IsString, Max, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
   @IsString()
-  @MinLength(3)
-  @MaxLength(20)
   readonly name: string;
   @IsString()
   readonly email: string;
